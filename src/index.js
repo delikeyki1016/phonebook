065f09f95f2ccd162cb672afd702e398d9efe578
delikeyki1016 <149133856+delikeyki1016@users.noreply.github.com>
2024-08-28 22:20:38 +0900

    주석 추가

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ root.render(
     // 1. npm install redux react-redux
     // 2. redux 폴더 생성 - store.js 생성
     //    reducer 폴더 생성(여러개의 리듀서가 있을 수 있기때문에) - reducer.js 생성(store에서 import)
+    // 3. useDispatch를 이용해서 reducer에 action을 던져줌
+    // 4. useSelector를 이용해서 reducer의 state를 읽어옴
     <Provider store={store}>
         <App />
     </Provider>
